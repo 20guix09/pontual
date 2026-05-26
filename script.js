43fee6119adfe32e2e0177822fe04ee640e52981
20guix09 <guilhermelimadejesus.estudos@gmail.com>
2026-05-25 23:00:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuToggle = document.getElementById('menuToggle');
+const navMenu = document.getElementById('navMenu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-menu a, .footer a').forEach(link => {
+  link.addEventListener('click', () => navMenu.classList.remove('active'));
+});
